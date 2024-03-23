1bfee3002f698020e6ef7c43473f7681b63af296
LukeCreech <lukecreech11@gmail.com>
2024-03-23 16:20:11 -0400

    add second recipe

--- milestone2.sql
+++ milestone2.sql
@@ -156,3 +156,48 @@ VALUES (1, 1);
 
 INSERT INTO Comment (username, recipeID, commentText)
 VALUES ("lukecreech", 1, "This is a really great and healthy recipe!");
+
+
+-- Insert Mac and Cheese https://www.budgetbytes.com/extra-cheesy-homemade-mac-and-cheese/
+INSERT INTO Recipe (recipeName, descr, cookTime)
+VALUES ("Mac and Cheese", "This incredibly creamy and cheesy homemade mac and cheese is great on its own or as a jumping off point for your own creation.", 25);
+
+INSERT INTO Creates (username, recipeID)
+VALUES ("lukecreech", 2);
+
+INSERT INTO Instruction (recipeID, stepNumber, step)
+VALUES (2, 1, "Cook the macaroni according to the package directions (boil for 7-8 minutes, or until tender). Drain the macaroni in a colander."),
+(2, 2, "Place the butter, flour, and onion powder in a medium pot. Whisk and heat over medium until the butter is melted and the mixture begins to bubble. Once bubbling, continue to whisk and cook for one minute."),
+(2, 3, "After cooking the butter and flour roux, whisk in the milk. Continue to cook and whisk over medium heat until the milk begins to gently simmer, at which point it will thicken to the consistency of heavy cream or gravy."),
+(2, 4, "Turn the burner off. Season the white sauce with the salt and hot sauce."),
+(2, 5, "Begin to add the shredded cheddar, one handful at a time, whisking it in until completely melted before adding more. Continue until all of the cheese has been melted into the sauce. If the sauce becomes too cold to melt the cheese, place it back over low heat only briefly to continue melting the cheese. Overheating the sauce can cause it to break and become grainy."),
+(2, 6, "Add the cooked and drained pasta to the pot with the cheese sauce and stir to combine. Serve the mac and cheese hot and enjoy!");
+
+INSERT INTO Ingredient (recipeID, ingredientName, cost)
+VALUES (2, "8 oz. macaroni", 0.67),
+(2, "3 Tbsp butter", 0.30),
+(2, "3 Tbsp all-purpose flour", 0.03),
+(2, "1/2 tsp onion powder", 0.05),
+(2, "2 cups whole milk", 0.84),
+(2, "1/2 tsp hot sauce", 0.02),
+(2, "1/2 tsp salt ", 0.03),
+(2, "8 oz. block sharp cheddar, shredded", 2.25);
+
+INSERT INTO Categorized (categoryID, recipeID)
+VALUES (1, 2),
+(3, 2);
+
+INSERT INTO Rating (username, recipeID, score)
+VALUES ("lukecreech", 2, 4.6);
+
+INSERT INTO Favorite (recipeID, username)
+VALUES (2, "lukecreech");
+
+INSERT INTO Photo (photoURL)
+VALUES ("https://www.budgetbytes.com/wp-content/uploads/2021/12/Homemade-Mac-and-Cheese-above-800x533.jpg");
+
+INSERT INTO Displays (recipeID, photoID)
+VALUES (2, 2);
+
+INSERT INTO Comment (username, recipeID, commentText)
+VALUES ("lukecreech", 2, "Love this Mac and Cheese, it is so cheap!");
